13cfc4fabbef62bac80c824d8c6cc1e775281db1
vixxjy <ajorvictor48@gmail.com>
2022-07-20 12:16:42 +0100

    [Laboratory] samples verification and previous updates implemented.

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,6 @@
-
 //export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 //export const url = '/api/'
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU4MzU2ODQ3fQ.yFiOtW0ulw6TMpAAS3WRpP-TIlyQiFIMsHA8jC64D5wEHONiDqsBMhuUtfAxR1jYy1HhHTdzcUY4c0u-SWCPoQ';
+
