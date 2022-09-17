9d1802e0ffd9a9d8b46cae57f442af232d39e9d1
vixxjy <ajorvictor48@gmail.com>
2022-09-17 23:42:20 +0100

    [Laboratory] viral load results fix

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -3,9 +3,8 @@ export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 export const apiUrl =  '/api/v1/'
 
-
 /*
 export const apiUrl =  'http://localhost:8282/api/v1/';
 export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzMzU5MTcyfQ.mRfr3P71OnQEpOlYAJ0G2ZokaTwuiFBbaop6Dq2wSwKYGgwaWk28S5CiY1QI3rIECpCmbw9hJH-KXmPyIJNniw';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzNDgzNDQzfQ.qCECM0ba0-nq9_jOI3PTcasNqMKRagxTk9opQjcQt6HNj3wn4k2I9vucTiENSMw51Fc7r1QfmBiJPr_teVLVtg';
 */
